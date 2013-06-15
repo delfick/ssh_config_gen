eb0b4016493839b9c3832bd5b817086385c47f95
Stephen Moore <stephen@delfick.com>
2013-06-15 22:47:57 +1000

    upping version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 # Setup the project
 setup(
       name = "ssh-config-gen"
-    , version = '0.1'
+    , version = '0.2'
     , packages = ['ssh_config_gen']
 
     , install_requires =
